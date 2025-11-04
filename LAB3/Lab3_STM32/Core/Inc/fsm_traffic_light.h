512ee6bb1f8df728284dcb10259d4d7bb5a1e996
HieuVan <hieu27718@gmail.com>
2025-11-04 15:01:09 +0700

    Lab3 update

---
+++ LAB3/Lab3_STM32/Core/Inc/fsm_traffic_light.h
@@ -0,0 +1,18 @@
+/*
+ * fsm_traffic_light.h
+ *
+ *  Created on: Oct 16, 2025
+ *      Author: ADMIN
+ */
+#ifndef INC_FSM_TRAFFIC_LIGHT_H_
+#define INC_FSM_TRAFFIC_LIGHT_H_
+
+#include "global.h"
+#include "button.h"
+#include "main.h"
+#include "display7SEG.h"
+#include "software_timer.h"
+
+void fsm_traffic_light();
+
+#endif /* INC_FSM_TRAFFIC_LIGHT_H_ */
---
+++ LAB3/Lab3_STM32/Core/Src/global.c
@@ -0,0 +1,14 @@
+/*
+ * global.c
+ *
+ *  Created on: Oct 16, 2025
+ *      Author: ADMIN
+ */
+
+#include "global.h"
+
+int STATUS = INIT;
+
+int RED = 5;
+int AMBER = 2;
+int GREEN = 3;
